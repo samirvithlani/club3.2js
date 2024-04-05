e0aff318ed249040975173713c515ad2fdbc42c0
samirvithlani <samir.vithlani83955@gmail.com>
2024-04-05 14:45:10 +0530

    mouse

---
+++ dom-events/event2.js
@@ -0,0 +1,16 @@
+const changeColor = ()=>{
+   
+    const mydiv = document.getElementById('mydiv');
+    mydiv.style.backgroundColor = 'green';
+}
+const changeColor1 = ()=>{
+   
+    const mydiv = document.getElementById('mydiv');
+    mydiv.style.backgroundColor = 'red';
+}
+
+
+const makeCircle =() =>{
+    const mydiv = document.getElementById('mydiv');
+    mydiv.style.borderRadius = '50%';
+}
