dfd2ac47edcc001be601ce2921c046b904646d48
samirvithlani <samir.vithlani83955@gmail.com>
2024-04-30 19:18:06 +0530

    tags

--- dom-events/byclass/byclass1.js
+++ dom-events/byclass/byclass1.js
@@ -3,7 +3,10 @@ function changeColor(){
 
     var box = document.getElementsByClassName('box');
     console.log(box)
+    //box[0].style.backgroundColor = 'green';
 
-
+    for(let i=0;i<box.length;i++){
+        box[i].style.backgroundColor = 'green';
+    }
 
 }
---
+++ dom-events/bytags/bytags.js
@@ -0,0 +1,10 @@
+function changeText(){
+
+
+    var h1 = document.getElementsByTagName("h1")
+    for(var i=0;i<h1.length;i++){
+        h1[i].innerHTML = 'Hacked...';
+        h1[i].style.color = 'red';
+    }
+
+}
