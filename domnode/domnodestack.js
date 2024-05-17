dc869c9f09d2f947f99a26508b580178ced84bd1
samirvithlani <samir.vithlani83955@gmail.com>
2024-05-17 14:55:02 +0530

    add

--- domnode/domnodestack.js
+++ domnode/domnodestack.js
@@ -1,1 +1,31 @@
-var data = [1,2,3,4,5,6]
+var data = [1, 2, 3, 4, 5, 6];
+const box = document.getElementById('box');
+const button = document.getElementById('btn');
+const removeButton = document.getElementById('removeBtn');
+
+
+
+button.addEventListener('click', function() {
+    if (data.length > 0) {
+        const lastElement = data.pop(); // Remove and get the last element of the array
+        const p = document.createElement('p'); // Create a new paragraph element
+        p.innerHTML = lastElement; // Set the innerHTML of the paragraph to the last element
+        box.appendChild(p); // Append the paragraph to the box
+    }
+    else{
+        alert('No more data to display');
+    }
+});
+
+removeButton.addEventListener('click', function() {
+    const lastChild = box.lastElementChild; 
+    console.log(lastChild);
+    if (lastChild) {
+        const removedValue = parseInt(lastChild.textContent); 
+        data.push(removedValue); 
+        box.removeChild(lastChild); 
+    }
+    else{
+        alert('No more data to remove');
+    }
+});
