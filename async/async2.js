0d8eaa6332af2c9a06f0d4b45d74cc2f1baa4dd0
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-05 14:32:56 +0530

    nested promise

---
+++ async/async2.js
@@ -0,0 +1,33 @@
+const payment = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        message: "Payment Done",
+        data: {
+          id: 1,
+          amount: 1000,
+        },
+      });
+    }, 3000);
+  });
+};
+
+const receipt = async () => {
+  console.log("Starting payment..");
+  const receiptData = await payment(); // payment
+  console.log("payment end..");
+
+  console.log("receipt starts.");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(receiptData);
+    }, 3000);
+  });
+};
+const printreceipt = async () => {
+  const recp = await receipt();
+
+  console.log(recp);
+};
+
+printreceipt();
