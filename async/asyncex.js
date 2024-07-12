4faa94c6bf0e24debf44fd0eb500a9b36238a12d
samirvithlani <samir.vithlani83955@gmail.com>
2024-07-12 13:32:37 +0530

    promise

---
+++ async/asyncex.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+        id:1,
+        name:"Amit"
+    },
+    {
+        id:2,
+        name:"Rahul"
+    },
+    {
+        id:3,
+        name:"Raj"
+    },
+    {
+        id:4,
+        name:"Ravi"
+    }
+]
+
+
+const fetchUsers = () =>{
+
+
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(() => {
+            resolve(users);
+        }, 3000);
+    })
+
+
+}
+
+const printUsers = async () =>{
+
+
+    const main = document.getElementById("main");
+    const loader = document.getElementById("loading");
+    loader.innerHTML = "Loading....";
+    const allUsers = await fetchUsers();
+    loader.innerHTML = "";
+    for(let i =0;i<allUsers.length;i++){
+        const p = document.createElement("p");
+        p.innerHTML = allUsers[i].id + " "+allUsers[i].name;
+        main.appendChild(p);
+    }
+
+
+}
+printUsers();
